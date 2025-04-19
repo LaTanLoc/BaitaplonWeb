61f5ba95de9884ed7dbb7752e43904ab45a40720
LaTanLoc <26a4041202@hvnh.edu.vn>
2025-04-19 16:33:28 +0700

    .

--- JS/trangchu.js
+++ JS/trangchu.js
@@ -91,7 +91,7 @@
 
     form.addEventListener("submit", function (e) {
       e.preventDefault();
-      alert("Đặt lịch khám thành công! Chúng tôi sẽ liên lạc để xác nhận lịch hẹn trong 24h. \nCảm ơn bạn đã sử dụng dịch vụ của Nha Khoa Smile.");
+      alert("Đặt lịch hẹn thành công! Chúng tôi sẽ liên lạc để xác nhận lịch hẹn trong 24h. \nCảm ơn bạn đã sử dụng dịch vụ của Nha Khoa Smile.");
       form.reset();
     });
   });
